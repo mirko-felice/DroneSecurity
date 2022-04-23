a70f798d50915502a45fc70c26a12df042a0468d
Mirko Felice <mirko.felice@studio.unibo.it>
2022-04-23 14:35:10 +0200

    chore: change simulator to test warning and critical situations

--- drone-system/src/main/resources/it/unibo/dronesecurity/dronesystem/drone/proximitySimulator.py
+++ drone-system/src/main/resources/it/unibo/dronesecurity/dronesystem/drone/proximitySimulator.py
@@ -1,10 +1,13 @@
+import os
 import time
 import sys
+import secrets
 import random as rand
 
+r = secrets.SystemRandom(os.urandom(8))
 # Simulating Data Stream
 while True:
-    print(str(rand.random()))
+    print(str(r.uniform(20, 70)))
 
     sys.stdout.flush()
     time.sleep(0.3)
