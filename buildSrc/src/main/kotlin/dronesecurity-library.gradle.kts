0c91f90b51b72e0973bd412bf7145ea34ac3fdfb
Maxim Derevyanchenko <maxim.derevyanchenko@studio.unibo.it>
2022-07-22 15:59:41 +0200

    build: refactor project properties creation

--- buildSrc/src/main/kotlin/dronesecurity-library.gradle.kts
+++ buildSrc/src/main/kotlin/dronesecurity-library.gradle.kts
@@ -120,39 +120,50 @@ signing {
 }
 
 tasks {
-    val propertiesFile = file(projectDir.path + File.separator + "project.properties")
+    val propertiesFile = file(
+            (if (System.getenv("CI") == true.toString())
+                rootDir
+            else
+                projectDir)
+            .path + File.separator + "project.properties")
 
     register("createCerts") {
         doFirst {
             if (System.getenv("CI") == true.toString()) {
-            val droneCertFileName = "Drone.cert.pem"
-            val privateKeyFileName = "Drone.private.key.pem"
-            val rootCAFileName = "root-CA.pem"
-            val certFolderPath = mkdir("certs")
-            val droneCert = file(certFolderPath.path + File.separator + droneCertFileName)
-            val privateKey = file(certFolderPath.path + File.separator + privateKeyFileName)
-            val rootCA = file(certFolderPath.path + File.separator + rootCAFileName)
-            val username = if (certFolderPath.path.contains(File.separator + "user-application" + File.separator)) {
-                "DroneSecurity: User"
+                val droneCertFileName = "Drone.cert.pem"
+                val privateKeyFileName = "Drone.private.key.pem"
+                val rootCAFileName = "root-CA.pem"
+                val certFolderPath = mkdir("certs")
+                val droneCert = file(certFolderPath.path + File.separator + droneCertFileName)
+                val privateKey = file(certFolderPath.path + File.separator + privateKeyFileName)
+                val rootCA = file(certFolderPath.path + File.separator + rootCAFileName)
+                val username = if (certFolderPath.path.contains(File.separator + "user-application" + File.separator)) {
+                    "DroneSecurity: User"
+                } else {
+                    "DroneSecurity: Drone"
+                }
+
+                droneCert.appendText(System.getenv("DRONE_CERT"))
+                privateKey.appendText(System.getenv("PRIVATE_KEY"))
+                rootCA.appendText(System.getenv("ROOT_CA"))
+                val endpoint = System.getenv("ENDPOINT")
+
+                val projectProperties = Properties()
+                projectProperties.setProperty("certsFolderPath", certFolderPath.path + File.separator)
+                projectProperties.setProperty("certificateFile", droneCertFileName)
+                projectProperties.setProperty("privateKeyFile", privateKeyFileName)
+                projectProperties.setProperty("certificateAuthorityFile", rootCAFileName)
+                projectProperties.setProperty("endpoint", endpoint)
+                projectProperties.setProperty("clientID", username)
+                val outputStream = propertiesFile.outputStream()
+                projectProperties.store(outputStream, "Connection settings")
+                outputStream.close()
             } else {
-                "DroneSecurity: Drone"
-            }
-
-            droneCert.appendText(System.getenv("DRONE_CERT"))
-            privateKey.appendText(System.getenv("PRIVATE_KEY"))
-            rootCA.appendText(System.getenv("ROOT_CA"))
-            val endpoint = System.getenv("ENDPOINT")
-
-            val projectProperties = Properties()
-            projectProperties.setProperty("certsFolderPath", certFolderPath.path + File.separator)
-            projectProperties.setProperty("certificateFile", droneCertFileName)
-            projectProperties.setProperty("privateKeyFile", privateKeyFileName)
-            projectProperties.setProperty("certificateAuthorityFile", rootCAFileName)
-            projectProperties.setProperty("endpoint", endpoint)
-            projectProperties.setProperty("clientID", username)
-            val outputStream = propertiesFile.outputStream()
-            projectProperties.store(outputStream, "Connection settings")
-            outputStream.close()
+                copy {
+                    from(rootDir)
+                    include("project.properties")
+                    into(projectDir.path)
+                }
             }
         }
     }
@@ -161,10 +172,9 @@ tasks {
         doLast {
             if (System.getenv("CI") == true.toString()) {
                 val certFolderPath = file(projectDir.path + File.separator + "certs")
-                propertiesFile.delete()
-
                 certFolderPath.deleteRecursively()
             }
+            propertiesFile.delete()
         }
     }
 
